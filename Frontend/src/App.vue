ef4a821561a06bdd716b45c23bfc6c3a29b61c6d
maht-noreht <139751248+maht-noreht@users.noreply.github.com>
2025-03-10 21:47:29 +0800

    Frontend Vue setup

---
+++ Frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import dashboard from './components/dashboard.vue'
+</script>
+
+<template>
+
+    <dashboard />
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+
+</style>
